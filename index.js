b354217694fe2d2069506088fbdcab730a1039b6
ak-gee <75403489+ak-gee@users.noreply.github.com>
2021-05-04 15:55:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {createJwt, authenticate, permit}= require("./auth");
 const bcryptjs = require("bcryptjs");
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
-// const dbUrl = "mongodb://localhost:27017";
+ 
 
 
 //movie
@@ -17,9 +17,9 @@ dotenv.config();
 const dbUrl = process.env.DBURL ;
 
 app.use(cors({
-    // origin:"http://127.0.0.1:5500"
-    // origin:"https://pizza-anp-delivery.netlify.app"
-    origin:"http://localhost:3000"
+  
+     origin:"https://quizzical-clarke-2b1ad7.netlify.app"
+   
 }))
 
 let students = [];
@@ -29,21 +29,21 @@ app.post("/userlogin",async (req,res) => {
     try{
         let client =  await mongoClient.connect(dbUrl)
         let db = client.db("Movie");
-        console.log(req.body.email);
+    
         let user = await db.collection("users").findOne({"email":req.body.email});
         var response={};
       
         client.close();
-        console.log(user);
+      
         if(user){
             let result = await bcryptjs.compare(req.body.password,user.password)
-            console.log(result);
+          
             if (result){
                 const token = await createJwt({id:user._id})
-                console.log(token);
+               
                 
             res.json({
-                // message:"Password Vefiried:Success",
+                 
                 message:true,
                 token
             });
@@ -56,14 +56,14 @@ app.post("/userlogin",async (req,res) => {
         }
       
     }else{
-        // res.json({message:"User Doesnt Exist"})
+    
         res.json({message:false})
     }
-    console.log("Response:"+ response.message,response.token)
+  
 }
     catch(error){
-        console.log(error);
-        result.json({
+     
+        res.json({
             // message:"Error USer logging in"
             message:false
         })
@@ -75,18 +75,18 @@ app.post("/adminlogin",async (req,res) => {
     try{
         let client =  await mongoClient.connect(dbUrl)
         let db = client.db("Movie");
-        console.log(req.body.email);
-        let user = await db.collection("admins").findOne({"email":req.body.email});
+   
+        let user = await db.collection("admins").findOne({"adminemail":req.body.adminemail});
         var response={};
       
         client.close();
-        console.log(user);
+        
         if(user){
             let result = await bcryptjs.compare(req.body.adminpwd,user.adminpwd)
-            console.log(result);
+          
             if (result){
                 const token = await createJwt({id:user._id})
-                console.log(token);
+              
                 
             res.json({
                 // message:"Password Vefiried:Success",
@@ -96,22 +96,22 @@ app.post("/adminlogin",async (req,res) => {
             response = res.json;
         }else{
          
-            // res.json({message:"Password Authentication Failed"})
+          
             res.json({message:false})
             response = res.json;
         }
       
     }else{
-        // res.json({message:"User Doesnt Exist"})
+ 
         res.json({message:false})
     }
-    console.log("Response:"+ response.message,response.token)
+   
 }
     catch(error){
-        console.log(error);
+        
         res.json({
             message:"Error USer logging in"
-            // message:false
+             
         })
 
     }
@@ -124,22 +124,20 @@ app.post("/adminregister",async (req,res) => {
         let salt = await bcryptjs.genSalt(10);
         let hash = await bcryptjs.hash(req.body.adminpwd,salt);
         req.body.adminpwd =  hash;
-        console.log(salt,hash)
-        // alert("reg")
-        // let id = mongodb.ObjectID(req.params.id);
+   
         let student = await db.collection("admins").insertOne(req.body);
-        // alert(req.body)
+       
         client.close();
         res.json({
-            // message:"Admin registered in the collection"
+             
             message:true
         });
-        console.log("Admin reg");
+     
     }
     catch(error){
-        console.log(error);
+        
         res.json({
-            // message:"Error registering Admin"
+         
             message:false
         })
 
@@ -153,19 +151,17 @@ app.post("/userregister",async (req,res) => {
         let salt = await bcryptjs.genSalt(10);
         let hash = await bcryptjs.hash(req.body.password,salt);
         req.body.password =  hash;
-        console.log(salt,hash)
-        // alert("reg")
-        // let id = mongodb.ObjectID(req.params.id);
+      
         let student = await db.collection("users").insertOne(req.body);
-        // alert(req.body)
+       
         client.close();
         res.json({
             message:"User registered in the collection"
         });
-        console.log("user reg");
+       
     }
     catch(error){
-        console.log(error);
+        
         res.json({
             message:"Error registering student"
         })
@@ -183,7 +179,7 @@ app.get("/allmovies",[authenticate],async(req,res) => {
     res.json(items);
     }
     catch(error){
-        console.log(error);
+      
         res.json({
             message:"Error getting all items"
         })
@@ -192,17 +188,17 @@ app.get("/allmovies",[authenticate],async(req,res) => {
 })
 
 app.get("/viewgenres",[authenticate],async(req,res) => {
-    // res.json(students)
+    
     try{
     let client = await mongoClient.connect(dbUrl);
     let db = client.db("Movie");
     let items = await db.collection("movies").find({"itemCode":"1"}).toArray();
     client.close();
-    console.log(items);
+     
     res.json(items);
     }
     catch(error){
-        console.log(error);
+        
         res.json({
             message:"Error getting pizzas"
         })
@@ -210,34 +206,17 @@ app.get("/viewgenres",[authenticate],async(req,res) => {
     }
 })
 
-app.get("/pizzabase",async(req,res) => {
-    // res.json(students)
-    try{
-    let client = await mongoClient.connect(dbUrl);
-    let db = client.db("Movie");
-    let items = await db.collection("movies").find().toArray();
-    client.close();
-    console.log(items);
-    res.json(items);
-    }
-    catch(error){
-        console.log(error);
-        res.json({
-            message:"Error getting pizzabase"
-        })
 
-    }
-})
 
 app.delete("/deletemovie",[authenticate],async (req,res) => {
     try{
-        console.log("Delete")
+        
         let client =  await mongoClient.connect(dbUrl)
         let db = client.db("Movie");
-        console.log(req.body.itemName);
+        
         let read = await db.collection("movies").deleteOne({itemName:req.body.itemName});
         var response={};
-        console.log(read);
+      
       
         client.close();
         res.json({
@@ -245,7 +224,7 @@ app.delete("/deletemovie",[authenticate],async (req,res) => {
         })
     }
     catch(error){
-        console.log(error);
+        
         res.json({
             message:"Error deleting 1 item"
         })
@@ -253,22 +232,33 @@ app.delete("/deletemovie",[authenticate],async (req,res) => {
     }
 })
 
-app.post("/addnewmovie",async (req,res) => {
+
+app.post("/addnewmovie",[authenticate],async (req,res) => {
     try{
-        console.log("Here")
+        
         let client =  await mongoClient.connect(dbUrl)
         let db = client.db("Movie");
-        console.log(req.body.email);
-        let user = await db.collection("movies").insertOne(req.body);
+      
+        let mov = await db.collection("movies").insertOne(req.body);
+
         var response={};
       
-        client.close();
+        
         res.json({
             message:"Success"
         })
+       
+       
+        let ins = await db.collection("tickets").insertOne({"movieName":req.body.movieName,"movieTime":"11am","movieDate":'',"ticketLeft":req.body.ticketquantity});
+        ins = await db.collection("tickets").insertOne({"movieName":req.body.movieName,"movieTime":"2pm","movieDate":'',"ticketLeft":req.body.ticketquantity});
+        ins = await db.collection("tickets").insertOne({"movieName":req.body.movieName,"movieTime":"7pm","movieDate":'',"ticketLeft":req.body.ticketquantity});
+        ins = await db.collection("tickets").insertOne({"movieName":req.body.movieName,"movieTime":"11pm","movieDate":'',"ticketLeft":req.body.ticketquantity});
+        
+        client.close();
+   
     }
     catch(error){
-        console.log(error);
+       
         res.json({
             message:"Error posting 1 item"
         })
@@ -276,7 +266,69 @@ app.post("/addnewmovie",async (req,res) => {
     }
 })
 
-app.put("/movie/:id",async (req,res) => {
+app.post("/tickets",[authenticate],async (req,res) => {
+    try{
+   
+        var upd_val,upd_tkt;
+        var client =  await mongoClient.connect(dbUrl)
+        let db = client.db("Movie");
+        let ticketsLeft = await db.collection("tickets").findOne({"movieName":req.body.movieName,
+        "movieTime":req.body.movieTime,"movieDate":req.body.movieDate});
+       if (ticketsLeft != null)
+        
+        {
+            upd_val = ticketsLeft.ticketLeft-req.body.numberoftickets;
+            upd_tkt = ticketsLeft.ticketBooked + req.body.numberoftickets;
+ 
+
+        }
+       else
+        {
+            upd_val = 100-req.body.numberoftickets;
+            upd_tkt = req.body.numberoftickets
+        }
+      
+
+        let upd = await db.collection("tickets").updateOne({"movieName":req.body.movieName,
+        "movieTime":req.body.movieTime,"movieDate":req.body.movieDate},//"ticketBooked":req.body.numberoftickets
+        {
+            $set:
+            {
+                "ticketBooked":upd_tkt,
+                "ticketLeft":upd_val
+            }
+        },
+        { upsert: true }
+            );
+    
+
+      
+        // let mov = await db.collection("tickets").updateOne({movieName:req.body.movieName,movieDate:req.body.movieDate,movieTime:req.body.movieTime},{$set:{ticketquantity:{ticketsLeft}});
+
+        
+        res.json({
+            message:"Success"
+        })
+            
+       
+        
+   
+    }
+    catch(error){
+         
+        res.json({
+            message:"Error posting 1 item"
+        })
+
+    }
+    finally{
+
+        client.close();
+    }
+})
+
+
+app.put("/movie/:id",[authenticate],async (req,res) => {
     try{
         let client =  await mongoClient.connect(dbUrl)
         let db = client.db("Movie");
@@ -286,7 +338,7 @@ app.put("/movie/:id",async (req,res) => {
         res.json(student);
     }
     catch(error){
-        console.log(error);
+       
         res.json({
             message:"Error getting Movie"
         })
@@ -294,7 +346,7 @@ app.put("/movie/:id",async (req,res) => {
     }
 })
 
-app.delete("/movie/:id",async (req,res) => {
+app.delete("/movie/:id",[authenticate],async (req,res) => {
     try{
         let client =  await mongoClient.connect(dbUrl)
         let db = client.db("Movie");
@@ -304,7 +356,7 @@ app.delete("/movie/:id",async (req,res) => {
         res.json(student);
     }
     catch(error){
-        console.log(error);
+    
         res.json({
             message:"Error getting Movie"
         })
@@ -322,7 +374,7 @@ app.get("/movie/:id",async (req,res) => {
         res.json(student);
     }
     catch(error){
-        console.log(error);
+      
         res.json({
             message:"Error getting student"
         })
@@ -331,7 +383,7 @@ app.get("/movie/:id",async (req,res) => {
 })
 
 app.get("/movies",[authenticate,permit(1,2,3)],async(req,res) => {
-    // res.json(students)
+ 
     try{
     let client = await mongoClient.connect(dbUrl);
     let db = client.db("Movie");
@@ -340,7 +392,7 @@ app.get("/movies",[authenticate,permit(1,2,3)],async(req,res) => {
     res.json(students);
     }
     catch(error){
-        console.log(error);
+      
         res.json({
             message:"Error getting all Movies"
         })
@@ -360,7 +412,7 @@ app.post("/movie",async (req,res) => {
         })
     }
     catch(error){
-        console.log(error);
+      
         res.json({
             message:"Error posting 1 movie"
         })
@@ -370,5 +422,5 @@ app.post("/movie",async (req,res) => {
 
 let port = 5000;
 app.listen(process.env.PORT|| port,() =>{
-console.log("Listening on port: "+ port)
+//console.log("Listening on port: "+ port)
 });
